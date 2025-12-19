cebfdcd5c7bfa602f6fbbaa307102711c0b03583
Stephan Güter <51406643+StephanGuter@users.noreply.github.com>
2025-12-19 17:15:28 -0300

    Atualização 20251219

---
+++ cap02/js/ex2_1.js
@@ -0,0 +1,12 @@
+// Programa JavaScript que exibe o nome do usuário informado pelo usuário no campo de edição
+
+// Cria referência ao form e ao elemento h3 (onde será exibida a resposta)
+const frm = document.querySelector("form");
+const resp = document.querySelector("h3");
+
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e) => {
+    const nome = frm.inNome.value   // obtém o nome digitado no form
+    resp.innerText = `Olá ${nome}`  // exibe a resposta do programa
+    e.preventDefault()              // evita envio do form
+})
