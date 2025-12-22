25580d76790fc74097ba04e2fc686d886256514e
Stephan Güter <51406643+StephanGuter@users.noreply.github.com>
2025-12-21 23:48:12 -0300

    Atualização 20251221

--- cap02/js/ex2_2.js
+++ cap02/js/ex2_2.js
@@ -1,3 +1,5 @@
+// Minha solução prévia
+/*
 const frm = document.querySelector("form")
 
 const inFilme = document.querySelector("input[type='text']")
@@ -7,7 +9,6 @@ const outFilme = document.querySelector("h3")
 const outDuracao = document.getElementById("outDuracao")
 
 frm.addEventListener("submit", (e) => {
-    
     const minutos = inDuracao.value
     let mensagem = ""
 
@@ -20,4 +21,24 @@ frm.addEventListener("submit", (e) => {
     outFilme.innerText = `Filme: ${inFilme.value}`
     outDuracao.innerText = mensagem
     e.preventDefault()
+})
+*/
+
+// cria referência ao form e aos elementos h3 e h4 (resposta)
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
+
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+frm.addEventListener("submit", (e) => {
+    const titulo = frm.inTitulo.value       // obtém o conteúdo dos campos
+    const duracao = Number(frm.inDuracao.value)
+
+    const horas = Math.floor(duracao / 60)  // arredonda para baixo resultado
+    const minutos = duracao % 60            // obtém o resto da divisão
+
+    resp1.innerText = titulo                // exibe as respostas
+    resp2.innerText = `${horas} hora(s) e ${minutos} minuto(s)`
+
+    e.preventDefault()                      // evita envio do form
 })
